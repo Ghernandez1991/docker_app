40726dbccbfcb8ee1cd21480e17af1becf854c70
Ghernandez1991 <hernandez.gabriel59@yahoo.com>
2024-12-14 17:24:03 -0600

    formatting

--- app.py
+++ app.py
@@ -1,67 +1,59 @@
-
-
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, MetaData, Table
-from sqlalchemy.orm import Session
-import sqlite3
 import pandas as pd
-import os
-from pathlib  import Path
-from src.models import db, County, Offender, Words
+from pathlib import Path
 
 app = Flask(__name__)
 
 
-db_path = Path('/app/data/executions.sqlite')
-full_db_path =f"sqlite:///{db_path}"
+db_path = Path("/app/data/executions.sqlite")
+full_db_path = f"sqlite:///{db_path}"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = full_db_path
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-
+app.config["SQLALCHEMY_DATABASE_URI"] = full_db_path
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
 db = SQLAlchemy(app)
 
 
 # create Flask Routes
-@app.route("/",  methods=["GET"])
+@app.route("/", methods=["GET"])
 def pie():
     import random
-    offender_df = pd.read_sql_table(
-        'offender', full_db_path)
+
+    offender_df = pd.read_sql_table("offender", full_db_path)
 
     global offender_dictionary
-    offender_dictionary = offender_df.to_dict('index')
+    offender_dictionary = offender_df.to_dict("index")
     random_offender = random.choice(list(offender_dictionary.values()))
 
-    return render_template('pie.html', random_offender=random_offender)
+    return render_template("pie.html", random_offender=random_offender)
 
 
 @app.route("/words")
 def words():
 
-    words_df = pd.read_sql_table(
-        'words', full_db_path)
+    words_df = pd.read_sql_table("words", full_db_path)
 
     # words_df.head()
 
-    list_of_words = words_df['Most_Spoken_Words'].to_list()
-    count_of_words = words_df['Count_of_Words'].to_list()
+    list_of_words = words_df["Most_Spoken_Words"].to_list()
+    count_of_words = words_df["Count_of_Words"].to_list()
 
-    words_dictionary = {"Most_Spoken_Words": list_of_words,
-                        "Count_of_Words": count_of_words}
+    words_dictionary = {
+        "Most_Spoken_Words": list_of_words,
+        "Count_of_Words": count_of_words,
+    }
 
     return jsonify(words_dictionary)
 
 
 @app.route("/county")
 def county():
-    county_df = pd.read_sql_table(
-        'county', full_db_path)
-    county_df = county_df.drop(['index'], axis=1)
+    county_df = pd.read_sql_table("county", full_db_path)
+    county_df = county_df.drop(["index"], axis=1)
 
-    county_dictionary = county_df.to_dict('records')
+    county_dictionary = county_df.to_dict("records")
 
     return jsonify(county_dictionary)
 
@@ -69,16 +61,15 @@ def county():
 @app.route("/offenders")
 def index():
 
-    offender_df = pd.read_sql_table(
-        'offender', full_db_path)
+    offender_df = pd.read_sql_table("offender", full_db_path)
 
     offender_df.head()
     global offender_dictionary
-    offender_dictionary = offender_df.to_dict('index')
+    offender_dictionary = offender_df.to_dict("index")
 
     return jsonify(offender_dictionary)
 
 
 if __name__ == "__main__":
 
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
--- src/models.py
+++ src/models.py
@@ -4,19 +4,20 @@ from flask_sqlalchemy import SQLAlchemy
 # Initialize the SQLAlchemy object
 db = SQLAlchemy()
 
+
 class County(db.Model):
-    __tablename__ = 'county'
+    __tablename__ = "county"
 
     Index = db.Column(db.Integer)
     County = db.Column(db.String, primary_key=True)
     Count = db.Column(db.Integer)
 
     def __repr__(self):
-        return '<County %r>' % (self.Index)
+        return "<County %r>" % (self.Index)
 
 
 class Offender(db.Model):
-    __tablename__ = 'offender'
+    __tablename__ = "offender"
 
     Index = db.Column(db.Integer)
     Execution = db.Column(db.Integer, primary_key=True)
@@ -30,15 +31,15 @@ class Offender(db.Model):
     County = db.Column(db.String)
 
     def __repr__(self):
-        return '<Offender %r>' % (self.Index)
+        return "<Offender %r>" % (self.Index)
 
 
 class Words(db.Model):
-    __tablename__ = 'words'
+    __tablename__ = "words"
 
     Index = db.Column(db.Integer, primary_key=True)
     Most_Spoken_Words = db.Column(db.String)
     Count_of_Words = db.Column(db.Integer)
 
     def __repr__(self):
-        return '<Words %r>' % (self.Index)
+        return "<Words %r>" % (self.Index)
