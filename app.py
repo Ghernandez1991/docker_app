80c691e9f5ea38ecc1848716be958a379fd4c2af
Ghernandez1991 <hernandez.gabriel59@yahoo.com>
2024-12-20 10:24:44 -0600

    updated render template

--- app.py
+++ app.py
@@ -32,13 +32,17 @@ def index():
     base_url = "https://www.google.com/search"
     first_name = random_offender.get("First_Name")
     last_name = random_offender.get("Last_Name")
+    last_name_for_tdcj = last_name.split(",", 1)[0]
     formatted_url = (
         f"{base_url}?q={first_name + " " + last_name + " " + "texas death row"}"  # noqa
     )
+    tdcj_link = f"https://www.tdcj.texas.gov/death_row/dr_info/{last_name_for_tdcj}{first_name}last.html"  # noqa
     return render_template(
         "index.html",
         random_offender=random_offender,
-        formatted_url=formatted_url,  # noqa
+        formatted_url=formatted_url,
+        tdcj_link=tdcj_link,
+        last_name_for_tdcj=last_name_for_tdcj,  # noqa
     )
 
 
--- dash_app/dash_app.py
+++ dash_app/dash_app.py
@@ -1,4 +1,6 @@
 from dash import Dash, dcc, html
+import pandas as pd
+from pathlib import Path
 
 
 def create_dash_app(flask_server):
@@ -9,28 +11,30 @@ def create_dash_app(flask_server):
         __name__, server=flask_server, url_base_pathname="/dashboard/"
     )  # noqa
 
+    pie_chart_dictionary = get_pie_chart_data()
+
     # Dash layout with a static pie chart
     dash_app.layout = html.Div(
         [
-            html.H1("Simple Pie Chart", style={"textAlign": "center"}),
+            html.H1("Offender Last Statements", style={"textAlign": "center"}),
             dcc.Graph(
-                id="simple-pie-chart",
+                id="Most Common File Words",
                 figure={
                     "data": [
                         {
                             "type": "pie",
-                            "labels": [
-                                "Category A",
-                                "Category B",
-                                "Category C",
-                            ],  # noqa
-                            "values": [450, 300, 150],
+                            "labels": list(
+                                pie_chart_dictionary.get("Most_Spoken_Words")
+                            ),  # noqa
+                            "values": list(
+                                pie_chart_dictionary.get("Count_of_Words")
+                            ),  # noqa
                             "textinfo": "label+percent",
                             "textposition": "inside",
                         }
                     ],
                     "layout": {
-                        "title": {"text": "Static Pie Chart", "x": 0.5},
+                        "title": {"text": "Most Common Final Words", "x": 0.5},
                         "height": 600,
                         "width": 800,
                     },
@@ -40,3 +44,43 @@ def create_dash_app(flask_server):
     )
 
     return dash_app
+
+
+def get_pie_chart_data():
+
+    db_path = Path("/app/data/executions.sqlite")
+    full_db_path = f"sqlite:///{db_path}"
+    words_df = pd.read_sql_table("words", full_db_path)
+
+    list_of_words = words_df["Most_Spoken_Words"].to_list()
+    count_of_words = words_df["Count_of_Words"].to_list()
+
+    words_dictionary = {
+        "Most_Spoken_Words": list_of_words,
+        "Count_of_Words": count_of_words,
+    }
+
+    return words_dictionary
+
+
+def create_links():
+
+    db_path = Path("/app/data/executions.sqlite")
+    full_db_path = f"sqlite:///{db_path}"
+    offenders = pd.read_sql_table("offender", full_db_path)
+
+    last_names = offenders["Last_Name"].to_list()
+    first_names = offenders["First_Name"].to_list()
+
+    len(last_names)
+    len(first_names)
+    zipped = zip(last_names, first_names)
+
+    last_statement_list = []
+    for item in zipped:
+        last_name = item[0].split(",", 1)[0]
+        first_name = item[1]
+        base_url = f"https://www.tdcj.texas.gov/death_row/dr_info/{last_name}{first_name}last.html"  # noqa
+        last_statement_list.append(base_url)
+
+    return last_statement_list
